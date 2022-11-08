f787982f0fed42a9971d13c2e78e0a1cc471985c
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-11-08 17:21:09 +0600

    init

--- ami_coding_pari_na/khoj_the_search/models.py
+++ ami_coding_pari_na/khoj_the_search/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User as DefaultDjUser
 
 # Create your models here.
 class comma_separated_integers(models.Model):
+    user_id = models.ForeignKey(DefaultDjUser, on_delete=models.CASCADE)
     list_itself = models.CharField(max_length=80)
 
     def __str__(self):
--- ami_coding_pari_na/khoj_the_search/views.py
+++ ami_coding_pari_na/khoj_the_search/views.py
@@ -5,17 +5,25 @@ from . models import comma_separated_integers
 
 def home_page(request):
     if request.method == 'POST' and request.POST.get('string_input'):
+
+        #getting the form data 
         form_inserted_list =request.POST.get('string_input')
-        form_search_value = request.POST.get('inputed_search_key')
+        form_searched_value = int(request.POST.get('inputted_search_key'))
         comma_list = comma_separated_integers()
-        
+
+        # sorting the list descending 
         the_int_list = map(int, form_inserted_list.split(","))
-        print(type(the_int_list))
-        #print(the_int_list)
-        sorted_the_list = sorted(a_int_list,reverse=True)
-        #print(sorting_the_list)
+        sorted_the_list = sorted(the_int_list,reverse=True)
+
+
+        #saving the data into database 
         comma_list.list_itself = sorted_the_list
         comma_list.save()
+
+        #returning the result
         boolean_val = bool()
+        if form_searched_value in sorted_the_list:
+            boolean_val = True
         return render(request, 'home.html',{"boolean_val":boolean_val})
+    
     return render(request, 'home.html')
