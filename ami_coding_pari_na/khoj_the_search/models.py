e4e01850f55a507902a8f24749915b617c8d46bf
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-11-08 16:54:02 +0600

    init

--- ami_coding_pari_na/khoj_the_search/models.py
+++ ami_coding_pari_na/khoj_the_search/models.py
@@ -4,6 +4,15 @@ from django.db import models
 class comma_separated_integers(models.Model):
     list_itself = models.CharField(max_length=80)
 
-    def save(self, *args, **kwargs):
-        self.list_itself = sorted(list_itself, reverse=True)
-        super(comma_separated_integers, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.list_itself
+    
+
+    # def save(self, *args, **kwargs):
+    #     self.list_itself = sorted(self.list_itself, reverse=True)
+    #     # sorting the array/list in the overriding save 
+    #     print(self.list_itself)
+    #     super(comma_separated_integers, self).save(*args, **kwargs)
+
+
+
--- ami_coding_pari_na/khoj_the_search/views.py
+++ ami_coding_pari_na/khoj_the_search/views.py
@@ -5,11 +5,17 @@ from . models import comma_separated_integers
 
 def home_page(request):
     if request.method == 'POST' and request.POST.get('string_input'):
+        form_inserted_list =request.POST.get('string_input')
+        form_search_value = request.POST.get('inputed_search_key')
         comma_list = comma_separated_integers()
-        test_print = request.POST.get('string_input')
-        print(test_print)
-        #comma_list.list_itself = request.POST.get('string_input')
-        #comma_list.save()
+        
+        the_int_list = map(int, form_inserted_list.split(","))
+        print(type(the_int_list))
+        #print(the_int_list)
+        sorted_the_list = sorted(a_int_list,reverse=True)
+        #print(sorting_the_list)
+        comma_list.list_itself = sorted_the_list
+        comma_list.save()
         boolean_val = bool()
         return render(request, 'home.html',{"boolean_val":boolean_val})
     return render(request, 'home.html')
