549ff36401dadec5ec5b20464f9c18234c0ff6df
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2024-05-18 11:26:02 +0600

    init

---
+++ next-todo-app/src/app/page.js
@@ -0,0 +1,20 @@
+// import Image from "next/image";
+// import styles from "../styles/page.module.css";
+
+// export default function Home() {
+//   return (
+//     <main className={styles.main}>
+//       <h1> Hello this is an nextjs project </h1>
+//     </main>
+//   );
+// }
+
+import styles from "./styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <h1>Welcome to Next.js!</h1>
+    </main>
+  );
+}
